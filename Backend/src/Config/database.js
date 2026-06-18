fc193283c1b70999d4634fe2cc894d60cec8e592
biib1 <beatrizcgb.021@gmail.com>
2026-06-18 20:20:23 +0100

    database

--- Backend/src/Config/database.js
+++ Backend/src/Config/database.js
@@ -2,14 +2,20 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
+  process.env.DB_DATABASE,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 5432),
     dialect: 'postgres',
-    logging: false,
+    port: 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false 
+      }
+    },
+    logging: false
   }
 );
 
