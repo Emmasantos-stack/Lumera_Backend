c13c27c4b9a4c07b99d16a3ecc891cb4f3d2b15d
biib1 <beatrizcgb.021@gmail.com>
2026-06-18 20:26:54 +0100

    Revert "database"
    This reverts commit fc193283c1b70999d4634fe2cc894d60cec8e592.

--- Backend/src/Config/database.js
+++ Backend/src/Config/database.js
@@ -2,20 +2,14 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
+  process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 5432),
     dialect: 'postgres',
-    port: 5432,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false 
-      }
-    },
-    logging: false
+    logging: false,
   }
 );
 
